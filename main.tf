78a12807abf70e20d0b595882b31d691a59cc0e2
Yash Khemani <khemani@m13.home.seva.cafe>
2021-03-27 01:20:14 +0000

    add vnet and subnet

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
   required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
+    azurerm            = {
+      source           = "hashicorp/azurerm"
     }
   }
-  required_version = ">= 0.13"
+  required_version     = ">= 0.13"
 }
 
 provider "azurerm" {
@@ -12,10 +12,28 @@ provider "azurerm" {
 }
 
 resource azurerm_resource_group arg {
-  name     = "${var.prefix}-resource-group"
-  location = var.location
+  name                 = "${var.prefix}-resource-group"
+  location             = var.location
 
-  tags = {
-    owner = var.owner
+  tags                 = {
+    owner              = var.owner
   }
+}
+
+resource azurerm_virtual_network vnet {
+  name                 = "${var.prefix}-vnet"
+  resource_group_name  = azurerm_resource_group.arg.name
+  location             = azurerm_resource_group.arg.location
+  address_space        = var.vnet_address_space
+
+  tags                 = {
+    owner              = var.owner
+  }
+}
+
+resource azurerm_subnet subnet0 {
+  name                 = "${var.prefix}-subnet0"
+  resource_group_name  = azurerm_resource_group.arg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.subnet0_address_prefixes
 }
--- variables.tf
+++ variables.tf
@@ -10,3 +10,13 @@ variable location {
   type   = string
   default = "West US"
 }
+
+variable vnet_address_space {
+  type    = list
+  default = ["10.0.0.0/16"]
+}
+
+variable subnet0_address_prefixes {
+  type    = list
+  default = ["10.0.0.0/24"]
+}
