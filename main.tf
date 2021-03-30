40312180304b0f559e190d8ec651f88262b35f40
Yash Khemani <khemani@m13.home.seva.cafe>
2021-03-30 13:48:58 +0000

    reorg

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    azurerm            = {
-      source           = "hashicorp/azurerm"
-    }
-  }
-  required_version     = ">= 0.13"
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource azurerm_resource_group arg {
   name                 = "${var.prefix}-resource-group"
   location             = var.location
